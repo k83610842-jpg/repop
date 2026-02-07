272824c77fa1dd9ae25b65230bb62ee0ad7708da
Kamila <k83610842@gmail.com>
2026-02-07 11:12:24 +0500

    add 4.py 5.py 6.py

---
+++ Practice2/if_else/1.py
@@ -0,0 +1,24 @@
+#1
+a = 45
+b = 240
+if b > a:
+  print("b is greater than a")
+#2
+n=2
+if n > 0:
+  print("The n is positive")
+  #3
+a = 45
+b = 240
+if b > a:
+print("b is greater than a") # you will get an error'
+#4
+age = 254
+if age >= 240:
+  print("rr")
+  print("hhhh")
+  print("sss")
+#5
+log= True
+if log:
+  print("Welcome back!")
---
+++ Practice2/if_else/3.py
@@ -0,0 +1,39 @@
+#1
+a = 299
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+#2
+a = 299
+b = 33
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+#3
+n=87
+if n% 2 == 0:
+  print("The n is even")
+else:
+  print("The n is odd")
+#4
+temperature = 24
+if temperature > 30:
+  print("It's hot outside!")
+elif temperature > 20:
+  print("It's warm outside")
+elif temperature > 10:
+  print("It's cool outside")
+else:
+  print("It's cold outside!")
+#5
+username = "Serik"
+
+if len(username) > 0:
+  print(f"Welcome, {username}!")
+else:
+  print("Error: Username cannot be empty")
