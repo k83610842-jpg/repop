04226b91cd7333e51f8ecadd2625ac51d767d481
k83610842-jpg <k83610842@gmail.com>
2026-02-21 10:54:58 +0500

    Update 2.py

--- practice3/functions/2.py
+++ practice3/functions/2.py
@@ -1,1 +1,30 @@
+#1
+def func(num):
+  print(num + " gpa")
 
+my_function("3.5")
+my_function("2.3")
+my_function("4.0")
+#2
+def unct(name): # name is a parameter
+  print("Hello", name)
+
+my_function("Kmaila") # "Emil" is an argument
+#3
+def func(surname, name):
+  print(surname + " " + name)
+
+my_function("Kairat", "Barsa")
+#4
+def func(name = "friend"):
+  print("Hello", name)
+
+my_function("Kairat")
+my_function("Tom")
+my_function()
+#5
+def func(a, b):
+  print("I have a", a)
+  print("My", a + "'s name is", b)
+
+my_function(b = "Buddy", a = "dog")
