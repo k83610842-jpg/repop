3fb17618908e8c8b903d6b89799fe04a4e401c98
k83610842-jpg <k83610842@gmail.com>
2026-02-21 11:54:00 +0500

    Update 1.py

--- practice3/inheritance/1.py
+++ practice3/inheritance/1.py
@@ -1,1 +1,74 @@
+#1
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+x = Person("Kairat", "Barsa")
+x.printname()
+#2
+class Student(Person):
+  def __init__(self, fname, lname):
+    super().__init__(fname, lname)
+# 3
+class Animal:
+    def speak(self):
+        print("Some sound")
+class Dog(Animal):
+    pass
+Dog().speak()  
+
+# 4
+class Vehicle:
+    def wheels(self):
+        print("4 wheels")
+class Car(Vehicle):
+    pass
+Car().wheels()  
+#5
+class Fruit:
+    def color(self):
+        print("Unknown")
+class Apple(Fruit):
+    pass
+Apple().color()  
+#6
+class Animal:
+    def __init__(self, name):
+        self.name = name
+class Dog(Animal):
+    def __init__(self, name, breed):
+        super().__init__(name)
+        self.breed = breed
+Dog("Buddy", "Beagle")
+
+# 7
+class Vehicle:
+    def __init__(self, brand):
+        self.brand = brand
+class Car(Vehicle):
+    def __init__(self, brand, model):
+        super().__init__(brand)
+        self.model = model
+#8
+class Animal:
+    def speak(self):
+        print("Some sound")
+class Dog(Animal):
+    def speak(self):
+        print("Woof!")
+Dog().speak() 
+#9
+class Flyer:
+    def fly(self):
+        print("I can fly")
+class Swimmer:
+    def swim(self):
+        print("I can swim")
+class Duck(Flyer, Swimmer):
+    pass
+Duck().fly()  
+Duck().swim()
 
