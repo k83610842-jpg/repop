cc73311d33e77167a98fb3f1ab933e3d68ac9805
k83610842-jpg <k83610842@gmail.com>
2026-02-21 11:11:46 +0500

    Update 3.py

--- practice3/functions/3.py
+++ practice3/functions/3.py
@@ -1,1 +1,43 @@
+#1
+def func(*animals):
+  print("The fav animal is " + animals[1])
+
+my_function("dig", "cat")
+#2
+def func(*args):
+  print("Type:", type(args))
+  print("First argument:", args[0])
+  print("Second argument:", args[1])
+  print("All arguments:", args)
+
+my_function("animal", "cat")
+#3
+def func(greaat, *names):
+  for x in names:
+    print(greet, x)
+
+my_function("Hello", "dog", "cat")
+#4
+def funct(*numbers):
+  total = 0
+  for x in numbers:
+    total += x
+  return total
+
+print(my_function(1, 2, 3))
+print(my_function(8987, 77, 54, 32))
+print(my_function(79))
+#5
+def func(*numbers):
+  if len(numbers) == 0:
+    return None
+  max = numbers[0]
+  for x in numbers:
+    if x > max:
+      max = x
+  return max
+
+print(my_function(3, 7, 2, 9, 1))
+
+
 
