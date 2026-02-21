eafe654d4018a431c9585f0f1821f2153b2324d1
k83610842-jpg <k83610842@gmail.com>
2026-02-21 10:47:14 +0500

    Update 1.py

--- practice3/functions/1.py
+++ practice3/functions/1.py
@@ -1,1 +1,24 @@
+#1
+def func():
+  print("Hello")
+#2
+def func():
+  print("Hello")
+fun()
+#3
+def func():
+  print("Hello")
+func()
+func()
+#4
+def plus(x):
+  return (x + 32) 
 
+print(plus(77))
+print(plus(95))
+print(plus(50))
+#5
+def func():
+  return "Hello"
+
+print(func())
