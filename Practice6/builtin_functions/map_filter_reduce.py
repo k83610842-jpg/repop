b1f63d70cf158e033a74f31eda581e7b771a3247
k83610842-jpg <k83610842@gmail.com>
2026-03-21 12:15:42 +0500

    Update map_filter_reduce.py

--- Practice6/builtin_functions/map_filter_reduce.py
+++ Practice6/builtin_functions/map_filter_reduce.py
@@ -1,1 +1,21 @@
+#1
+l=list(map(int,input().split()))
+ev=list(map(lambda x:x*2,l))
+print(ev)
 
+l=list(map(int,input().split()))
+ev1=list(filter(lambda x:x%2==0,l))
+print(ev1)
+#2
+from functools import reduce
+res=reduce(lambda acc, x: acc + x, l)
+print(res)
+#3
+names=["Miras", "Bob"]
+ages=[38, 73]
+for a, b in zip(names, ages):
+    print(a, b)
+#4
+x=10
+print(type(x))
+isinstance(x, int)
