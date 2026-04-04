76327d1fc222ea6a81f5f352fedb1e15906d7bc9
k83610842-jpg <k83610842@gmail.com>
2026-04-04 12:32:04 +0500

    Create connect.py

---
+++ practice8/connect.py
@@ -0,0 +1,12 @@
+import psycopg2
+import config
+
+def get_connection():
+    conn = psycopg2.connect(
+        dbname=config.DB_NAME,
+        user=config.DB_USER,
+        password=config.DB_PASSWORD,
+        host=config.DB_HOST,
+        port=config.DB_PORT
+    )
+    return conn
