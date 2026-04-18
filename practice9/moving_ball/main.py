3d120117244fc60049f7b9311b28c5bfc5cf736f
k83610842-jpg <k83610842@gmail.com>
2026-04-18 12:11:16 +0500

    Update main.py

--- practice9/moving_ball/main.py
+++ practice9/moving_ball/main.py
@@ -1,1 +1,32 @@
-
+import pygame 
+import sys
+from m1 import Ball
+def main():
+    pygame.init()
+    W,H=600,400
+    screen=pygame.display.set_mode((W,H))
+    pygame.display.set_caption("moving ball game")
+    ball=Ball(W,H)
+    clock=pygame.time.Clock()
+    running=True
+    while running:
+        screen.fill((255,255,255))
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                running=False
+        keys=pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            ball.move("UP")
+        if keys[pygame.K_DOWN]:
+            ball.move("DOWN")
+        if keys[pygame.K_LEFT]:
+            ball.move("LEFT")
+        if keys[pygame.K_RIGHT]:
+            ball.move("RIGHT")
+        ball.draw(screen)
+        pygame.display.flip()
+        clock.tick(60)
+    pygame.quit()
+    sys.exit()
+if __name__=="__main__":
+    main()
